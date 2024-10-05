ea92b95d087c16b58d95d4bed0a6bd4765e1a2d6
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-10-05 11:45:59 +0500

    feat: build reducer file and add data

--- learn-context-api/src/reducer.js
+++ learn-context-api/src/reducer.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const reducer = () => {
-  return (
-    <div>reducer</div>
-  )
+const reducer = (state,action) => {
+    switch (action.type){
+        case "USER_LOGIN":{
+            return {...state,user:action.payload}
+        }
+        case "USER_LOGOUT":{
+            return {...state,user:null}
+        }
+        case "CHANGE_THEME":{
+            return {...state,darkTheme:!state.darkTheme}
+        }
+        default:{
+            return state
+        }
+    }
 }
 
 export default reducer
