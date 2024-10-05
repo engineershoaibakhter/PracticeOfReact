d774825bfcee0de68bb02dc4712a8ec2f9920e33
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-10-05 11:36:33 +0500

    feat: add new react file for context api

---
+++ learn-context-api/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentOne from './components/componentOne';
+import ComponentTwo from './components/componentTwo';
+
+function App() {
+  return (
+  <>
+  <ComponentOne />
+  <hr />
+  <ComponentTwo />
+  </>
+  );
+}
+
+export default App;
---
+++ learn-context-api/src/components/componentTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponentTwo = () => {
+  return (
+    <div>componentTwo</div>
+  )
+}
+
+export default ComponentTwo
